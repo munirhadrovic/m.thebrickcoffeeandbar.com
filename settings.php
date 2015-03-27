6f7335a1a939c8476413b683054ba753c24ff045
anes89cok <anes89cok@gmail.com>
2015-03-27 15:05:34 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="The Brick 531 Fifth Avenue South"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="FL 34102"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2396928755;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
